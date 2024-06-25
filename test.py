bb43fc67347110d7eb193e9fb535e68ee206953f
LC9032 <lichieh.chou94@gmail.com>
2024-06-25 15:08:17 +0200

    WIP...

--- test.py
+++ test.py
@@ -11,77 +11,108 @@ import numpy as np
 
 from processingDataset import ProcessingDataset
 
-file_path_l = './training_inputs_COL/left_data.pkl'
-file_path_r = './training_inputs_COL/right_data.pkl'
 
 
-def test():
-    process_dataset = ProcessingDataset()
+class Test:
+    def __init__(self):
+        self.imgFilePath = './preprocessing_dataset_COL/0024/left/0024_2m_0P_0V_-15H.jpg'
+        self.imgInfoPath = './preprocessing_dataset_COL/0024/info/left/0024_2m_0P_0V_-15H.txt'
+        # self.imgFilePath = './preprocessing_dataset_CelebA/0/left/anna-jackson-2.jpg'
+        # self.imgInfoPath = './preprocessing_dataset_CelebA/0/info/left/anna-jackson-2.txt'
 
-    # Define the generator and discriminator (assuming they are implemented as classes)
-    generator = Generator()
-    discriminator = Discriminator()
+        self.file_path_l = './training_inputs_COL/left_data.pkl'
+        self.file_path_r = './training_inputs_COL/right_data.pkl'
 
-    # Define the optimizers (same as used during training)
-    generator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
-    discriminator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
+    def loadData(self):
+        process_dataset = ProcessingDataset()
 
-    # Load the checkpoint
-    checkpoint_dir = './training_checkpoints'
-    checkpoint_prefix = os.path.join(checkpoint_dir, 'ckpt')
-    checkpoint = tf.train.Checkpoint(generator=generator,
-                                    discriminator=discriminator,
-                                    gen_optimizer=generator_optimizer,
-                                    disc_optimizer=discriminator_optimizer
-                                    )
+        # Load the test image
+        eye_landmarks = process_dataset.read_landmarks_from_txt(self.imgInfoPath )
+        landmarks = tf.cast(eye_landmarks, tf.float32)
+        landmarks = tf.expand_dims(landmarks, axis=0)
 
-    # Restore the latest checkpoint
-    checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
+        test_image = process_dataset.load_and_preprocess_image(self.imgFilePath)
+        test_image = tf.cast(test_image, tf.float32)
+        test_image = tf.expand_dims(test_image, axis=0)  # Add batch dimension
 
-    # Load the test image
-    eye_landmarks = process_dataset.read_landmarks_from_txt('./preprocessing_dataset_CelebA/0/info/left/anna-jackson-2.txt')
-    # eye_landmarks = process_dataset.read_landmarks_from_txt('./preprocessing_dataset_COL/0024/info/left/0024_2m_0P_0V_-15H.txt')
-    landmarks = tf.cast(eye_landmarks, tf.float32)
+        # Example target gaze direction
+        gaze_target = np.array([[0.0, 0.0]])  # Adjust as needed
+        gaze_target = tf.convert_to_tensor(gaze_target, dtype=tf.float32)
 
-    landmarks = tf.expand_dims(landmarks, axis=0)
+        gaze_target = tf.expand_dims(gaze_target, axis=0)
 
-    test_image_path = './preprocessing_dataset_CelebA/0/left/anna-jackson-2.jpg'
-    # test_image_path = './preprocessing_dataset_COL/0024/left/0024_2m_0P_0V_-15H.jpg'
-    test_image = process_dataset.load_and_preprocess_image(test_image_path)
+        pose = 0.0
+        pose = tf.convert_to_tensor(pose, dtype=tf.float32)
+        pose = tf.expand_dims(pose, axis=0)
 
-    test_image = tf.cast(test_image, tf.float32)
+        return test_image, pose, gaze_target, landmarks
 
-    test_image = tf.expand_dims(test_image, axis=0)  # Add batch dimension
+    def loadDataFromPKL(self):
+        process_dataset = ProcessingDataset()
 
-    # Example target gaze direction
-    gaze_target = np.array([[0.0, 0.0]])  # Adjust as needed
-    gaze_target = tf.convert_to_tensor(gaze_target, dtype=tf.float32)
+        batch_size = 64
 
-    gaze_target = tf.expand_dims(gaze_target, axis=0)
+        test_data = process_dataset.load_pickle_data(self.file_path_l)
+        test_dataset = process_dataset.create_dataset(test_data, batch_size)
 
-    pose = 0.0
-    pose = tf.convert_to_tensor(pose, dtype=tf.float32)
-    pose = tf.expand_dims(pose, axis=0)
+        for (img_t, p_t, gaze_target, landmarks_t), (img, p, gaze_real, landmarks) in test_dataset.take(1):
+            break
 
-    # Generate the output image
+        img = tf.cast(img, tf.float32)
+        img_t = tf.cast(img_t, tf.float32)
 
-    generated_image = generator(test_image, pose, gaze_target, landmarks, training=False)
+        gaze_target = tf.cast(gaze_target, tf.float32)
+        gaze_real = tf.cast(gaze_real, tf.float32)
 
-    test_image = (test_image + 1.0) / 2.0
-    generated_image = (generated_image + 1.0) / 2.0
+        return img, p, gaze_target, landmarks
 
-    fig, axes = plt.subplots(1, 2, figsize=(15, 5))
+
+    def plotIMGs(self, test_image, generated_image):
+        fig, axes = plt.subplots(1, 2, figsize=(15, 5))
+            
+        axes[0].imshow(test_image[0].numpy())
+        axes[0].set_title("Original Image")
+        axes[0].axis('off')
+
+        axes[1].imshow(generated_image[0].numpy())
+        axes[1].set_title("Generated Image")
+        axes[1].axis('off')
+
+        plt.savefig('./result_CA.png')
+        plt.close(fig)
+
+    def run(self):
+        # Define the generator and discriminator
+        generator = Generator()
+        discriminator = Discriminator()
+
+        # Define the optimizers
+        generator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
+        discriminator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
+
+        # Load the checkpoint
+        checkpoint_dir = './training_checkpoints'
+        checkpoint_prefix = os.path.join(checkpoint_dir, 'ckpt')
+        checkpoint = tf.train.Checkpoint(generator=generator,
+                                        discriminator=discriminator,
+                                        gen_optimizer=generator_optimizer,
+                                        disc_optimizer=discriminator_optimizer
+                                        )
+
+        # Restore the latest checkpoint
+        checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
+
+        # test_image, pose, gaze_target, landmarks = self.loadData()
+        test_image, pose, gaze_target, landmarks = self.loadDataFromPKL()
         
-    axes[0].imshow(test_image[0].numpy())
-    axes[0].set_title("Original Image")
-    axes[0].axis('off')
+        # Generate the output image
+        generated_image = generator(test_image, pose, gaze_target, landmarks, training=False)
 
-    axes[1].imshow(generated_image[0].numpy())
-    axes[1].set_title("Generated Image")
-    axes[1].axis('off')
+        test_image = (test_image + 1.0) / 2.0
+        generated_image = (generated_image + 1.0) / 2.0
 
-    plt.savefig('./result_CA.png')
-    plt.close(fig)
+        self.plotIMGs(test_image, generated_image)
 
 
-test()
+test = Test()
+test.run()
--- train.py
+++ train.py
@@ -11,107 +11,61 @@ import matplotlib.pyplot as plt
 import cv2
 
 import numpy as np
-file_path_l = './training_inputs_COL/left_data.pkl'
-file_path_r = './training_inputs_COL/right_data.pkl'
 
 
+class Train():
+    def __init__(self):
+        self.file_path_l = './training_inputs_COL/left_data.pkl'
+        self.file_path_r = './training_inputs_COL/right_data.pkl'
+        self.epochs = 1
 
-def train():
-    process_dataset = ProcessingDataset()
+    def run(self):
+        process_dataset = ProcessingDataset()
 
-    batch_size = 64
-    data_l = process_dataset.load_pickle_data(file_path_l)
-    data_r = process_dataset.load_pickle_data(file_path_r)
-    data = {**data_l, **data_r}
+        batch_size = 64
+        data_l = process_dataset.load_pickle_data(self.file_path_l)
+        data_r = process_dataset.load_pickle_data(self.file_path_r)
+        data = {**data_l, **data_r}
 
-    train_dataset = process_dataset.create_dataset(data, batch_size)
+        train_dataset = process_dataset.create_dataset(data, batch_size)
 
-    generator = Generator()
+        generator = Generator()
+        discriminator = Discriminator()
 
-    discriminator = Discriminator()
+        gan_model = GazeRedirectGAN(generator, discriminator)
+        gan_model.compile(
+            gen_optimizer=tf.keras.optimizers.Adam(learning_rate=0.0002, beta_1=0.9),
+            disc_optimizer=tf.keras.optimizers.Adam(learning_rate=0.0002, beta_1=0.5),
+            loss_fn = tf.keras.losses.MeanSquaredError()
+        )
 
-    generator_optimizer = tf.keras.optimizers.Adam(learning_rate=0.0002, beta_1=0.9)
+        # Define checkpoint directory and checkpoint objects
+        checkpoint_dir = './training_checkpoints'
+        checkpoint_prefix = os.path.join(checkpoint_dir, 'ckpt')
+        checkpoint = tf.train.Checkpoint(generator=gan_model.generator,
+                                        discriminator=gan_model.discriminator,
+                                        gen_optimizer=gan_model.gen_optimizer,
+                                        disc_optimizer=gan_model.disc_optimizer
+                                        )
 
-    gan_model = GazeRedirectGAN(generator, discriminator)#, discriminator, vgg_model)
-    gan_model.compile(
-        gen_optimizer=generator_optimizer,
-        disc_optimizer=tf.keras.optimizers.Adam(learning_rate=0.0002, beta_1=0.5),
-        loss_fn = tf.keras.losses.MeanSquaredError()
-    )
+        checkpoint_manager = tf.train.CheckpointManager(checkpoint, checkpoint_dir, max_to_keep=10)
 
-    # Define checkpoint directory and checkpoint objects
-    checkpoint_dir = './training_checkpoints'
-    checkpoint_prefix = os.path.join(checkpoint_dir, 'ckpt')
-    checkpoint = tf.train.Checkpoint(generator=gan_model.generator,
-                                    discriminator=gan_model.discriminator,
-                                    gen_optimizer=gan_model.gen_optimizer,
-                                    disc_optimizer=gan_model.disc_optimizer
-                                    )
-    # checkpoint = tf.train.Checkpoint(gan_model=gan_model, gen_optimizer=gan_model.gen_optimizer, disc_optimizer=gan_model.disc_optimizer)
+        # Restore the latest checkpoint if it exists
+        if checkpoint_manager.latest_checkpoint:
+            checkpoint.restore(checkpoint_manager.latest_checkpoint)
+            print(f'Restored from checkpoint: {checkpoint_manager.latest_checkpoint}')
+        else:
+            print('No checkpoint found, starting from scratch.')
 
-    # checkpoint_manager = tf.train.CheckpointManager(checkpoint, checkpoint_dir, max_to_keep=5)
-    checkpoint_manager = tf.train.CheckpointManager(checkpoint, checkpoint_dir, max_to_keep=10)
+        # Define a callback to save checkpoints
+        class CheckpointSaver(tf.keras.callbacks.Callback):
+            def on_epoch_end(self, epoch, logs=None):
+                checkpoint_manager.save()
+                print(f'\nCheckpoint saved at epoch {epoch + 1}')
 
-    # Restore the latest checkpoint if it exists
-    if checkpoint_manager.latest_checkpoint:
-        checkpoint.restore(checkpoint_manager.latest_checkpoint)
-        print(f'Restored from checkpoint: {checkpoint_manager.latest_checkpoint}')
-    else:
-        print('No checkpoint found, starting from scratch.')
-
-    # Define a callback to save checkpoints
-    class CheckpointSaver(tf.keras.callbacks.Callback):
-        def on_epoch_end(self, epoch, logs=None):
-            checkpoint_manager.save()
-            print(f'\nCheckpoint saved at epoch {epoch + 1}')
-
-    # Train the GAN model with checkpoint saving
-    epochs = 1
-
-    gan_model.fit(train_dataset, epochs=epochs, callbacks=[CheckpointSaver()])
+        gan_model.fit(train_dataset, epochs=self.epochs, callbacks=[CheckpointSaver()])
 
 
-    #########################################################################################################################################
-    # batch_size = 32
-    # test_data = process_dataset.load_pickle_data('./training_inputs_COL/left_data.pkl')
-    # test_dataset = process_dataset.create_dataset(test_data, batch_size)
-
-    # for (img_t, p_t, gaze_target, landmarks_t), (img, p, gaze_real, landmarks) in test_dataset.take(1):
-    #     break
-
-    # img = tf.cast(img, tf.float32)
-    # img_t = tf.cast(img_t, tf.float32)
-
-    # gaze_target = tf.cast(gaze_target, tf.float32)
-    # gaze_real = tf.cast(gaze_real, tf.float32)
-
-    # # Generate the target image using the generator
-
-    # # print(gaze_target)
-    # generated_image = generator(img, p, gaze_target, landmarks, training=False)
-
-    # # Convert images to the range [0, 1] for displaying
-    # img = (img + 1.0) / 2.0
-    # img_t = (img_t + 1.0) / 2.0
-    # generated_image = (generated_image + 1.0) / 2.0
-
-    # # Plot the original, target, and generated images
-    # fig, axes = plt.subplots(1, 3, figsize=(15, 5))
-
-    # axes[0].imshow(img[0].numpy())
-    # axes[0].set_title("Original Image")
-    # axes[0].axis('off')
-
-    # axes[1].imshow(img_t[0].numpy())
-    # axes[1].set_title("Target Image")
-    # axes[1].axis('off')
-
-    # axes[2].imshow(generated_image[0].numpy())
-    # axes[2].set_title("Generated Image")
-    # axes[2].axis('off')
-
-    # plt.savefig('./result.png')
-    # plt.close(fig)
-
-
-train()
+train = Train()
+train.run()
+print("Training Completed!!!")
